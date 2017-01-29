10f70cbba376de9aa759c1119e294db120c16361
ain2108 <ain.jesape@gmail.com>
2017-01-29 03:52:40 -0500

    HAve some data

---
+++ hw3/runtests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+./a.out 10 > data.txt
+./a.out 20 >> data.txt
+./a.out 30 >> data.txt
+./a.out 40 >> data.txt
+./a.out 50 >> data.txt
+./a.out 60 >> data.txt
+./a.out 100 >> data.txt
+./a.out 1000 >> data.txt
+./a.out 10000 >> data.txt
+./a.out 100000 >> data.txt
+./a.out 200000 >> data.txt
+./a.out 300000 >> data.txt
+./a.out 500000 >> data.txt
