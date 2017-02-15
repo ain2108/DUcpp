b43621479ae07934ae0d653c97b972fe3f390219
ain2108 <ain.jesape@gmail.com>
2017-02-14 23:02:23 -0500

    unrolled the dr/dc loops, achieved a small speedup

--- archhw1/opt.h
+++ archhw1/opt.h
@@ -7,33 +7,158 @@ for (int c = 0; c < ncols; c++) {
     	uint sum_G = 0;
     	uint sum_B = 0;
 
-    	for (int dc = -1; dc <= 1; dc++) {
-			
-			for (int dr = -1; dr <= 1; dr++) {
-	  			
-	  			int cc = c+dc;
-	  			int rr = r+dr;
-	  			
-	  			if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
-	    			uint x = ncols*rr+cc;
-	    			uint y = 3*(dc+1)+(dr+1);
-	    			assert(x < ncols*nrows);
-	    			assert(y < FILTER_BYTES);
-	    			temp = filt[y];
-	    			
-	    			// Increment the accumulators
-	    			sum_R += in[x].R * temp;
-	    			sum_G += in[x].G * temp;
-	    			sum_B += in[x].B * temp;
-	  			}
-			}
-      	}
+      	/* Unrolling the loop */
+      	int dc = -1;
+      	int dr = -1;
 
-    uint x = ncols*r+c;
-    uchar d = filt[FILTER_BYTES-1];
-    out[x].R = (float) sum_R / d;
-    out[x].G = (float) sum_G / d;
-    out[x].B = (float) sum_B / d;
+      	int cc = c+dc;
+		int rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = -1;
+		dr = 0;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = -1;
+		dr = 1;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 0;
+		dr = -1;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 0;
+		dr = 0;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 0;
+		dr = 1;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 1;
+		dr = -1;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 1;
+		dr = 0;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+		dc = 1;
+		dr = 1;
+		cc = c+dc;
+		rr = r+dr;
+		
+		if (rr >= 0 && cc >= 0 && rr < nrows && cc < ncols) {
+			uint x = ncols*rr+cc;
+			uint y = 3*(dc+1)+(dr+1);
+			
+			// Increment the accumulators
+			temp = filt[y];
+			sum_R += in[x].R * temp;
+			sum_G += in[x].G * temp;
+			sum_B += in[x].B * temp;
+		}
+
+
+    	uint x = ncols*r+c;
+    	uchar d = filt[FILTER_BYTES-1];
+    	out[x].R = (float) sum_R / d;
+    	out[x].G = (float) sum_G / d;
+    	out[x].B = (float) sum_B / d;
 
 	}
 }
