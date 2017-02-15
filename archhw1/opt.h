da9365a406ac24d815408ef7d366184088059b7a
ain2108 <ain.jesape@gmail.com>
2017-02-15 02:58:46 -0500

    me stupid forgot to swap the loop, at 2.4 speedup now

--- archhw1/opt.h
+++ archhw1/opt.h
@@ -218,9 +218,23 @@ c = ncols - 1;
 
 int temp;
 uchar d = filt[FILTER_BYTES-1];
+pixel 	t1, t2, t3,
+		t4, t5, t6,
+		t7, t8, t9;
 
-for (int c = 1; c < ncols - 1; c++) {
-	for (int r = 1; r < nrows - 1; r++) {
+t1 = in[0];
+t2 = in[1];
+t3 = in[2];
+t4 = in[ncols];
+t5 = in[ncols + 1];
+t6 = in[ncols + 2];
+t7 = in[ncols + ncols];
+t8 = in[ncols + ncols + 1];
+t9 = in[ncols + ncols + 2];
+
+
+for (int r = 1; r < nrows - 1; r++) {
+	for (int c = 1; c < ncols - 1; c++) {
     	
     	uint sum_R = 0;
     	uint sum_G = 0;
@@ -237,18 +251,16 @@ for (int c = 1; c < ncols - 1; c++) {
 
       	int cc = c+dc;
 		int rr = r+dr;
-		
-		
-			uint x = ncols*rr+cc;
+		uint x = ncols*rr+cc;
 
-			// one less operation here
-			uint y = 3*dc+dr+4;
-			
-			// Increment the accumulators
-			temp = filt[y];
-			sum_R += in[x].R * temp;
-			sum_G += in[x].G * temp;
-			sum_B += in[x].B * temp;
+		// one less operation here
+		uint y = 3*dc+dr+4;
+		
+		// Increment the accumulators
+		temp = filt[y];
+		sum_R += in[x].R * temp;
+		sum_G += in[x].G * temp;
+		sum_B += in[x].B * temp;
 		
 
 
@@ -259,15 +271,12 @@ for (int c = 1; c < ncols - 1; c++) {
 		dr = 1;
 		cc = c+dc;
 		rr = r+dr;
-		
-			x = ncols*rr+cc;
-			y = 3*dc+dr+4;
-			
-			// Increment the accumulators
-			temp = filt[y];
-			sum_R += in[x].R * temp;
-			sum_G += in[x].G * temp;
-			sum_B += in[x].B * temp;
+		x = ncols*rr+cc;
+		y = 3*dc+dr+4;
+		temp = filt[y];
+		sum_R += in[x].R * temp;
+		sum_G += in[x].G * temp;
+		sum_B += in[x].B * temp;
 		
 
 		//  .  .  x
@@ -277,15 +286,12 @@ for (int c = 1; c < ncols - 1; c++) {
 		dr = 1;
 		cc = c+dc;
 		rr = r+dr;
-		
 		x = ncols*rr+cc;
 		y = 3*dc+dr+4;
-			
-			// Increment the accumulators
-			temp = filt[y];
-			sum_R += in[x].R * temp;
-			sum_G += in[x].G * temp;
-			sum_B += in[x].B * temp;
+		temp = filt[y];
+		sum_R += in[x].R * temp;
+		sum_G += in[x].G * temp;
+		sum_B += in[x].B * temp;
 
 
 
