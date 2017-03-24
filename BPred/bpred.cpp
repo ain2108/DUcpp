1caea1a34dd927789c890feebd4ac4d8384f2d05
ain2108 <ain.jesape@gmail.com>
2017-03-24 02:36:20 -0400

    .

--- BPred/bpred.cpp
+++ BPred/bpred.cpp
@@ -165,9 +165,9 @@ void init_globals(){
   assert(local_counters != NULL);
   memset(local_counters, 0, sizeof(int) * rows * columns);
 
-  print_local_counters();
-  SET_COUNTER(1, 1, 10);
-  print_local_counters();
+  // print_local_counters();
+  // SET_COUNTER(1, 1, 10);
+  // print_local_counters();
 
 }
 
@@ -215,6 +215,7 @@ VOID Fini(int, VOID * v) {
 
     out.close();
 
+    print_local_counters();
     // Cleanuup
     free(local_counters);
 }
