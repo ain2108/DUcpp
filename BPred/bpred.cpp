77a08c6d67623404b4f0b498835a932ebe0c30ec
ain2108 <ain.jesape@gmail.com>
2017-03-24 02:42:15 -0400

    .

--- BPred/bpred.cpp
+++ BPred/bpred.cpp
@@ -149,6 +149,7 @@ void init_globals(){
 
   // We need to change the function pointer here for two bit counter
   if(n == TWO){
+    cout << "WARNING: Using 2-BIT counter.\n";
     DoBranch = DoBranch2BIT;
   }else{
     DoBranch = DoBranchGeneral;
@@ -170,9 +171,9 @@ void init_globals(){
   assert(local_counters != NULL);
   memset(local_counters, 0, sizeof(int) * rows * columns);
 
-  // print_local_counters();
-  // SET_COUNTER(1, 1, 10);
-  // print_local_counters();
+  print_local_counters();
+  SET_COUNTER(1, 1, 10);
+  print_local_counters();
 
 }
 
